38c0a51eece1fc6fef5735281505b72a3e838271
Kriss <kris1stsab@gmail.com>
2023-11-11 17:41:28 +0200

    google map api 0.1

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,10 +8,23 @@ import { API_KEY } from "../constants";
 
 function Homepage() {
   const [select, setSelect] = useState("Local");
+  const [markers, setMarkers] = useState([]);
+  const [lastLatPosition, setLatPosition] = useState('none')
+  const [lastLngPosition, setLngPosition] = useState('none')
+
+  const onMapClick = (event) => {
+    setLatPosition(event.latLng.lat())
+    setLngPosition(event.latLng.lng())
+    const newMarker = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    };
+    setMarkers([...markers, newMarker]);
+  }
 
   return (
-    <div className=" h-screen w-screen">
-      <nav className="flex justify-between w-screen border-b-4 border-black p-3 h-[61px]">
+    <div className=" h-screen w-screen bg-[url('websitebg.jpg')]">
+      <nav className="flex justify-between w-screen p-3 h-[61px] bg-white">
         <p className="text-3xl font-semibold flex">
           <img className=" w-7 h-7 m-1.5" src={heartImg}></img>
           HlpMe
@@ -19,51 +32,63 @@ function Homepage() {
         <p className="text-3xl font-semibold hover:cursor-pointer">Sign Up</p>
       </nav>
       <div className="h-[750px] flex items-center justify-center p-6">
-        <div className="flex items-center border-2 border-black p-12 rounded-xl">
+        <div className="flex items-center p-12 rounded-xl backdrop-blur-xl">
           <img src={helpImg} className=" h-[450px] w-[400px] rounded-2xl"></img>
-          <p className="flex-wrap m-4 max-w-[300px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minus alias iste aperiam labore a magni porro vel, doloribus maiores, architecto, quos sit voluptas eligendi facere laboriosam quas. Ratione, fugiat!</p>
+          <p className="flex-wrap m-4 max-w-[300px] text-white text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minus alias iste aperiam labore a magni porro vel, doloribus maiores, architecto, quos sit voluptas eligendi facere laboriosam quas. Ratione, fugiat!</p>
         </div>
       </div>
-      <div className="flex w-screen justify-around ">
-        <p className="py-8 px-20 bg-white hover:cursor-pointer border-black border-2 rounded-2xl" onClick={() => setSelect('Global')}>Global Help</p>
-        <p className="py-8 px-20 bg-white hover:cursor-pointer border-black border-2 rounded-2xl" onClick={() => setSelect("Local")}>Local Help</p>
+      <div className="flex w-screen justify-around pb-8">
+        <p className="py-8 px-20 bg-white hover:cursor-pointer rounded-2xl" onClick={() => setSelect('Global')}>Global Help</p>
+        <p className="py-8 px-20 bg-white hover:cursor-pointer rounded-2xl" onClick={() => setSelect("Local")}>Local Help</p>
       </div>
-      <LoadScript googleMapsApiKey={API_KEY}>
-        {select === 'Global' ? (
-          <div>
-            Global content
-          </div>
-        ) : null}
-        {select === 'Local' ? (
-          <div className="flex justify-center mt-10">
-            <GoogleMap
-              id="example-map"
-              mapContainerStyle={{ width: '400px', height: '400px', borderRadius: "24px", marginRight: "32px", }}
-              zoom={13}
-              center={{ lat: 49.839684, lng: 24.029716 }}
-            />
-            <div className="w-[400px] h-[400px] border-black border-4 rounded-2xl ml-8">
-              <h3 className="flex items-center justify-center m-3">My markers:</h3>
-              <ul className=" max-h-[350px] overflow-scroll">
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark1<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark2<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-                <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
-              </ul>
+      <div className="flex justify-center items-center h-screen w-screen bg-[url('websitebg2.jpg')]">
+        <LoadScript googleMapsApiKey={API_KEY}>
+          {select === 'Global' ? (
+            <div>
+              Global content
             </div>
-          </div>
-        ) : null}
-      </LoadScript>
-      <div className="my-6">We have to help each other</div>
-    </div>
+          ) : null}
+          {select === 'Local' ? (
+            <div className="flex justify-center py-12">
+              <GoogleMap
+                id="example-map"
+                mapContainerStyle={{ width: '600px', height: '600px', borderRadius: "24px", marginRight: "32px", }}
+                zoom={13}
+                center={{ lat: 49.839684, lng: 24.029716 }}
+                onClick={onMapClick}
+                {...markers.map((marker, index) => (
+                  <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+                ))}
+              />
+              <div className="w-[600px] h-[600px] border-black border-4 rounded-2xl ml-8 bg-white">
+                <h3 className="flex items-center justify-center m-3">My markers:</h3>
+                <ul className=" max-h-[250px] overflow-scroll">
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark1<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark2<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                  <li className="flex m-2 border-2 rounded-xl border-black p-1 justify-between">Mark3<img src={editImg} className="h-6 w-6 ml-4 hover:cursor-pointer"></img></li>
+                </ul>
+                <div>Last chosen position: {lastLatPosition} and {lastLngPosition}</div>
+                <div>Create marker</div>
+              </div>
+            </div>
+          ) : null
+          }
+        </LoadScript >
+      </div >
+      <div className="flex justify-center items-center text-4xl py-6"> By continuing to help others, you maintain your humanity.</div>
+    </div >
   );
 }
 export default Homepage;
