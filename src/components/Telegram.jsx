ca1bdac2a32d1878ea585ebfb5f0e10683cd2e3d
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-11-12 13:57:26 +0200

    123123

--- src/components/Telegram.jsx
+++ src/components/Telegram.jsx
@@ -1,10 +1,16 @@
 import useTelegramAuth from "@use-telegram-auth/hook";
-import { URL } from "../constants";
-import axios from "axios";
+import TelegramLogo from "./telegram-logo.svg";
+import toast, { Toaster } from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 async function handleSuccess(response) {
   localStorage.setItem("isAuthenticated", true);
   localStorage.setItem("userId", response["id"]);
+  toast.success("Successfully authenticated!", {
+    duration: 5000,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  document.getElementById("redirect").click();
 }
 
 function Telegram() {
@@ -23,15 +29,39 @@ function Telegram() {
 
   return (
     <>
+      <div>
+        <Toaster />
+      </div>
+
       <div className="flex h-[123px] items-center justify-center">
         <button
-          className="flex items-center justify-center rounded-xl bg-blue-500 px-4 py-2 text-2xl text-white duration-300 hover:bg-blue-700"
+          className="flex items-center justify-center 
+          rounded-xl border-2 bg-white px-4 py-2 text-2xl
+        text-slate-500/25 duration-300 hover:scale-125 
+        hover:border-slate-500/25 hover:bg-slate-500/25 hover:text-white
+        active:scale-100
+        "
           onClick={() => onAuth()}
         >
-          {isLoading ? "Authenticating..." : "Sign up with Telegram"}
+          {isLoading ? (
+            "Authenticating..."
+          ) : (
+            <span className="flex gap-3">
+              <img
+                className="w-[32px]"
+                src={TelegramLogo}
+                alt="Telegram logo"
+              />
+              Sign in with Telegram{" "}
+            </span>
+          )}
         </button>
       </div>
+      <Link to="/" id="redirect" hidden>
+        Go to New Route
+      </Link>
     </>
   );
 }
+
 export default Telegram;
