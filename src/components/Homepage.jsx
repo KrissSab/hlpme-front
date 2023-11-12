61e2eab4d9f82594adea47e6496d1c6f0abd0504
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-11-12 10:09:54 +0200

    123

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import heartImg from "../images/heart.svg";
 import helpImg from "../images/help.jpg";
 import { API_KEY } from "../constants";
-import { URL } from "../constants"
+import { URL } from "../constants";
 
 function Homepage() {
   const [select, setSelect] = useState("Local");
   const [marker, setMarker] = useState(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [lastLatPosition, setLatPosition] = useState(0)
-  const [lastLngPosition, setLngPosition] = useState(0)
+  const [lastLatPosition, setLatPosition] = useState(0);
+  const [lastLngPosition, setLngPosition] = useState(0);
   const [problemName, setProblemName] = useState(null);
   const [description, setDescription] = useState(null);
   const [isRequestMade, setIsRequestMade] = useState(false);
@@ -24,7 +24,7 @@ function Homepage() {
   function scrollDown() {
     window.scroll({
       top: window.innerHeight,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 
@@ -38,52 +38,61 @@ function Homepage() {
         coordinates: {
           latitude: lastLatPosition,
           longitude: lastLngPosition,
-        }
+        },
       };
-      console.log(data)
+      console.log(data);
       //const response = await axios.post(URL + "/local/dangers/create", data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (isRequestMade == false) {
-      axios.get(URL + "/local/dangers")
-        .then(response => {
+      axios
+        .get(URL + "/local/dangers")
+        .then((response) => {
           setIsRequestMade(true);
           const elements = response.data;
-          const ul = document.getElementById('myMarkersList');
+          const ul = document.getElementById("myMarkersList");
 
-          elements.forEach(element => {
-            const li = document.createElement('li');
+          elements.forEach((element) => {
+            const li = document.createElement("li");
             li.textContent = element.name;
-            li.classList.add('flex', 'm-2', 'border-2', 'rounded-xl', 'border-black', 'p-1', 'justify-between');
+            li.classList.add(
+              "flex",
+              "m-2",
+              "border-2",
+              "rounded-xl",
+              "border-black",
+              "p-1",
+              "justify-between",
+            );
 
-            const img = document.createElement('img');
-            img.src = 'edit.svg';
-            img.classList = 'h-6 w-6 ml-4 hover:cursor-pointer';
+            const img = document.createElement("img");
+            img.src = "edit.svg";
+            img.classList = "h-6 w-6 ml-4 hover:cursor-pointer";
 
             li.appendChild(img);
             ul.appendChild(li);
           });
         })
-        .catch(error => {
-          console.error('Помилка запиту:', error);
+        .catch((error) => {
+          console.error("Помилка запиту:", error);
         });
     }
   }, [isRequestMade]);
 
   const onMapClick = (event) => {
-    setLatPosition(event.latLng.lat())
-    setLngPosition(event.latLng.lng())
+    setLatPosition(event.latLng.lat());
+    setLngPosition(event.latLng.lng());
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
     };
     setMarker(newMarker);
     setIsDropdownOpen(false);
-  }
+  };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -95,73 +104,124 @@ function Homepage() {
 
   return (
     <div className=" h-screen w-screen bg-khaki-green2">
-      <nav className="flex justify-between w-screen p-3 h-[61px] bg-beige2">
-        <p className="text-3xl font-semibold flex">
-          <img className=" w-7 h-7 m-1.5" src={heartImg}></img>
+      <nav className="flex h-[61px] w-screen justify-between bg-beige2 p-3">
+        <p className="flex text-3xl font-semibold">
+          <img className=" m-1.5 h-7 w-7" src={heartImg}></img>
           HlpMe
         </p>
-        <p className="text-3xl font-semibold hover:cursor-pointer">Sign Up</p>
+        <a href="/tg" className="text-3xl font-semibold hover:cursor-pointer">
+          Sign Up
+        </a>
       </nav>
-      <div className="h-[750px] flex items-center justify-center p-6">
-        <div className="flex items-center p-12 gap-[100px] justify-around">
+      <div className="flex h-[750px] items-center justify-center p-6">
+        <div className="flex items-center justify-around gap-[100px] p-12">
           <img src={helpImg} className=" h-[450px] w-[400px] rounded-2xl"></img>
-          <p className="flex-wrap m-4 max-w-[300px] text-center text-beige2 text-2xl font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minus alias iste aperiam labore a magni porro vel, doloribus maiores, architecto, quos sit voluptas eligendi facere laboriosam quas. Ratione, fugiat!</p>
+          <p className="m-4 max-w-[300px] flex-wrap text-center text-2xl font-medium text-beige2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            minus alias iste aperiam labore a magni porro vel, doloribus
+            maiores, architecto, quos sit voluptas eligendi facere laboriosam
+            quas. Ratione, fugiat!
+          </p>
           <img src={helpImg} className=" h-[450px] w-[400px] rounded-2xl"></img>
         </div>
       </div>
       <div className="flex w-screen justify-around pb-[62px]">
         <p
-          className="mt-[-50px] py-8 px-20 bg-beige2 hover:cursor-pointer rounded-2xl border-4 border-khaki-green2 text-3xl font-semibold hover:border-4 hover:border-black"
-          onClick={() => { setSelect('Global'); scrollDown() }}>
+          className="mt-[-50px] rounded-2xl border-4 border-beige2 bg-beige2 px-20 py-8 text-3xl font-semibold hover:cursor-pointer hover:border-4 hover:border-black"
+          onClick={() => {
+            setSelect("Global");
+            scrollDown();
+          }}
+        >
           Global Help
         </p>
         <p
-          className="mt-[-50px] py-8 px-20 bg-beige2 hover:cursor-pointer rounded-2xl border-4 border-khaki-green2 text-3xl font-semibold hover:border-4 hover:border-black"
-          onClick={() => { setSelect("Local"); scrollDown() }}>
+          className="mt-[-50px] rounded-2xl border-4 border-beige2 bg-beige2 px-20 py-8 text-3xl font-semibold hover:cursor-pointer hover:border-4 hover:border-black"
+          onClick={() => {
+            setSelect("Local");
+            scrollDown();
+          }}
+        >
           Local Help
         </p>
       </div>
-      <div className="flex justify-center items-center h-screen w-screen bg-beige2">
+      <div className="flex h-screen w-screen items-center justify-center bg-beige2">
         <LoadScript googleMapsApiKey={API_KEY}>
-          {select === 'Global' ? (
-            <div>
-              Global content
-            </div>
-          ) : null}
-          {select === 'Local' ? (
+          {select === "Global" ? <div>Global content</div> : null}
+          {select === "Local" ? (
             <div className="flex justify-center py-12">
               <GoogleMap
                 id="example-map"
-                mapContainerStyle={{ width: '600px', height: '600px', borderRadius: "24px", marginRight: "32px", }}
+                mapContainerStyle={{
+                  width: "600px",
+                  height: "600px",
+                  borderRadius: "24px",
+                  marginRight: "32px",
+                }}
                 zoom={13}
                 center={{ lat: 49.839684, lng: 24.029716 }}
                 onClick={onMapClick}
               >
-                {marker && <Marker position={{ lat: marker.lat, lng: marker.lng }} onClick={onMarkerClick} />}
+                {marker && (
+                  <Marker
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                    onClick={onMarkerClick}
+                  />
+                )}
               </GoogleMap>
-              <div className="w-[600px] h-[600px] border-black border-4 rounded-2xl ml-8 bg-white">
-                <h3 className="flex items-center justify-center m-2 text-2xl font-semibold">My markers:</h3>
-                <ul id="myMarkersList" className=" h-[300px] max-h-[250px] overflow-scroll"></ul>
-                <div className="flex justify-center items-center border-t-4 border-black text-lg font-medium">Last chosen position:
-                  <p className="p-1 mx-2 text-lg font-medium text-gray-900 w-[175px]">{lastLatPosition}</p>
+              <div className="ml-8 h-[600px] w-[600px] rounded-2xl border-4 border-black bg-white">
+                <h3 className="m-2 flex items-center justify-center text-2xl font-semibold">
+                  My markers:
+                </h3>
+                <ul
+                  id="myMarkersList"
+                  className=" h-[300px] max-h-[250px] overflow-scroll"
+                ></ul>
+                <div className="flex items-center justify-center border-t-4 border-black text-lg font-medium">
+                  Last chosen position:
+                  <p className="mx-2 w-[175px] p-1 text-lg font-medium text-gray-900">
+                    {lastLatPosition}
+                  </p>
                   and
-                  <p className="p-1 mx-2 text-lg font-medium text-gray-900 w-[170px]">{lastLngPosition}</p>
+                  <p className="mx-2 w-[170px] p-1 text-lg font-medium text-gray-900">
+                    {lastLngPosition}
+                  </p>
                 </div>
-                <div className="flex flex-col justify-around items-center ">
-                  <input type="text" placeholder="Problem name" className="m-2 border-2 border-black p-4 w-[550px] rounded-xl text-xl h-[40px]" onChange={(e) => { changeHandler(e, setProblemName) }} />
-                  <textarea placeholder="Description" className="m-2 border-2 border-black p-4 h-[100px] w-[550px] rounded-xl text-lg break-words" onChange={(e) => { changeHandler(e, setDescription) }}></textarea>
+                <div className="flex flex-col items-center justify-around ">
+                  <input
+                    type="text"
+                    placeholder="Problem name"
+                    className="m-2 h-[40px] w-[550px] rounded-xl border-2 border-black p-4 text-xl"
+                    onChange={(e) => {
+                      changeHandler(e, setProblemName);
+                    }}
+                  />
+                  <textarea
+                    placeholder="Description"
+                    className="m-2 h-[100px] w-[550px] break-words rounded-xl border-2 border-black p-4 text-lg"
+                    onChange={(e) => {
+                      changeHandler(e, setDescription);
+                    }}
+                  ></textarea>
                 </div>
-                <div onClick={submitData} className="flex justify-center items-center">
-                  <span className="px-12 py-4 border-khaki-green2 border-4 rounded-xl text-xl font-semibold bg-khaki-green2 text-beige2 hover:cursor-pointer hover:border-black">Create marker</span>
+                <div
+                  onClick={submitData}
+                  className="flex items-center justify-center"
+                >
+                  <span className="rounded-2xl border-8 border-khaki-green2 bg-khaki-green2 px-12 py-4 text-xl font-semibold text-beige2 duration-300 ease-out hover:cursor-pointer hover:border-black/25">
+                    Create marker
+                  </span>
                 </div>
               </div>
             </div>
-          ) : null
-          }
-        </LoadScript >
-      </div >
-      <div className="flex justify-center items-center text-4xl py-6 bg-khaki-green2 text-beige2"> By continuing to help others, you maintain your humanity.</div>
-    </div >
+          ) : null}
+        </LoadScript>
+      </div>
+      <div className="flex items-center justify-center bg-khaki-green2 py-6 text-4xl text-beige2">
+        {" "}
+        By continuing to help others, you maintain your humanity.
+      </div>
+    </div>
   );
 }
 export default Homepage;
